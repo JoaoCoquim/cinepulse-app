cd33de4b95f093f1daa05e983d2334d6f76ad59c
JoaoCoquim <joaocoquim@gmail.com>
2024-09-16 16:10:11 +0100

    Added error handling for YouTube API quota limit

--- js/youtube-api.js
+++ js/youtube-api.js
@@ -5,9 +5,21 @@ document.addEventListener('DOMContentLoaded', () => {
   const youtubeSearch = `${movieTitle} ${movieYear} movie trailer`;
 
   fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&q=${youtubeSearch}&key=${YOUTUBE_API_KEY}`)
-    .then(response => response.json())
-    .then(data => {
-      const videoId = data.items[0].id.videoId;  // Gets the first video result
-      document.getElementById('trailerPlayer').src = `https://www.youtube.com/embed/${videoId}`;
-    });
+      .then(response => response.json())
+      .then(data => {
+          if (data.error) {
+              // Handle the error if the response contains an error
+              console.error('YouTube API Error:', data.error.message);
+          } else {
+              // Check if there are video results
+              if (data.items && data.items.length > 0) {
+                  const videoId = data.items[0].id.videoId;  // Gets the first video result
+                  document.getElementById('trailerPlayer').src = `https://www.youtube.com/embed/${videoId}`;
+              }
+          }
+      })
+      .catch(error => {
+          // Catch and handle any errors that occur during the fetch
+          console.error('Error fetching YouTube data:', error);
+      });
 });
