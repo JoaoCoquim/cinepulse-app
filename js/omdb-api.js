e65b22d964cf26135fbba9335d130ab5b1fc4560
JoaoCoquim <joaocoquim@gmail.com>
2024-09-16 15:59:53 +0100

    Finished card logic and updated HTML elements

--- js/omdb-api.js
+++ js/omdb-api.js
@@ -1,35 +1,37 @@
-const OMDB_API_KEY = window.config.OMDB_API_KEY;
-const suggestions = document.getElementById('suggestions');
-const warnings = document.getElementById('warnings');
-const moviesWithTwoLetters = ["A.I.", "B.S.", "CQ", "D2", "Da", "Em", "F/X", "Go", "Ho!", "I.Q.", "If", "If....", "IO", "It", "Jo", "Pi", "No", "PK", "RV", "Up", "Us", "W.E."];
+document.addEventListener('DOMContentLoaded', () => {
 
-searchInput.addEventListener('input', function () {
-    const query = searchInput.value.trim();
-    let apiUrl = `https://www.omdbapi.com/?s=${encodeURIComponent(query)}&apikey=${OMDB_API_KEY}`;
-    let isTwoLetterMatch = false;
+    const OMDB_API_KEY = window.config.OMDB_API_KEY;
+    const suggestions = document.getElementById('suggestions');
+    const warnings = document.getElementById('warnings');
+    const moviesWithTwoLetters = ["A.I.", "B.S.", "CQ", "D2", "Da", "Em", "F/X", "Go", "Ho!", "I.Q.", "If", "If....", "IO", "It", "Jo", "Pi", "No", "PK", "RV", "Up", "Us", "W.E."];
 
-    // Checks for 2 letter movies and handle them with specific title search (?t=)
-    moviesWithTwoLetters.forEach(element => {
-        if (query.length >= 2 && query.toLowerCase() === element.toLowerCase()) {
-            apiUrl = `https://www.omdbapi.com/?t=${element}&apikey=${OMDB_API_KEY}`;
-            isTwoLetterMatch = true;  // Marks this as a single movie query (from the array)
-        }
-    });
+    searchInput.addEventListener('input', function () {
+        const query = searchInput.value.trim();
+        let apiUrl = `https://www.omdbapi.com/?s=${encodeURIComponent(query)}&apikey=${OMDB_API_KEY}`;
+        let isTwoLetterMatch = false;
 
-    if (query.length > 1) {
-        fetch(apiUrl)
-            .then(response => response.json())
-            .then(data => {
-                if (data.Response === "True") {
-                    suggestions.innerHTML = '';
-                    suggestions.style.display = 'block';
-                    warnings.style.display = 'none';
+        // Checks for 2 letter movies and handle them with specific title search (?t=)
+        moviesWithTwoLetters.forEach(element => {
+            if (query.length >= 2 && query.toLowerCase() === element.toLowerCase()) {
+                apiUrl = `https://www.omdbapi.com/?t=${element}&apikey=${OMDB_API_KEY}`;
+                isTwoLetterMatch = true;  // Marks this as a single movie query (from the array)
+            }
+        });
 
-                    // Process results from movies with 2 letters
-                    if (isTwoLetterMatch) {
-                        const li = document.createElement('li');
-                        li.classList.add('list-group-item');
-                        li.innerHTML = `
+        if (query.length > 1) {
+            fetch(apiUrl)
+                .then(response => response.json())
+                .then(data => {
+                    if (data.Response === "True") {
+                        suggestions.innerHTML = '';
+                        suggestions.style.display = 'block';
+                        warnings.style.display = 'none';
+
+                        // Process results from movies with 2 letters
+                        if (isTwoLetterMatch) {
+                            const li = document.createElement('li');
+                            li.classList.add('list-group-item');
+                            li.innerHTML = `
                             <div style="display: flex; align-items: center; padding: 10px; cursor: pointer;">
                                 <img src="${data.Poster}" alt="${data.Title} Poster" style="width: auto; height: 75px; margin-right: 10px; object-fit: cover;">
                                 <div>
@@ -37,15 +39,15 @@ searchInput.addEventListener('input', function () {
                                 </div>
                             </div>
                         `;
-                        li.dataset.imdbID = data.imdbID;
-                        li.addEventListener('click', () => selectMovie(data.imdbID));
-                        suggestions.appendChild(li);
-                    } else {
-                        // Process results from `?s=` (multiple movies)
-                        data.Search.forEach(movie => {
-                            const li = document.createElement('li');
-                            li.classList.add('list-group-item');
-                            li.innerHTML = `
+                            li.dataset.imdbID = data.imdbID;
+                            li.addEventListener('click', () => selectMovie(data.imdbID));
+                            suggestions.appendChild(li);
+                        } else {
+                            // Process results from `?s=` (multiple movies)
+                            data.Search.forEach(movie => {
+                                const li = document.createElement('li');
+                                li.classList.add('list-group-item');
+                                li.innerHTML = `
                             <div style="display: flex; align-items: center; padding: 10px; cursor: pointer;">
                                 <img src="${movie.Poster}" alt="${movie.Title} Poster" style="width: auto; height: 75px; margin-right: 10px; object-fit: cover;">
                                 <div>
@@ -53,108 +55,120 @@ searchInput.addEventListener('input', function () {
                                 </div>
                             </div>
                         `;
-                            li.dataset.imdbID = movie.imdbID;
-                            li.addEventListener('click', () => selectMovie(movie.imdbID));
-                            suggestions.appendChild(li);
-                        });
-                    }
+                                li.dataset.imdbID = movie.imdbID;
+                                li.addEventListener('click', () => selectMovie(movie.imdbID));
+                                suggestions.appendChild(li);
+                            });
+                        }
 
-                    // Handle other results
-                } else if (data.Error === "Too many results.") {
-                    warnings.innerHTML = `<p id="error">Too many results. Please check your syntax!</p>`;
-                    warnings.style.display = 'block';
-                    suggestions.style.display = 'none';
-                } else if (data.Error === "Movie not found!") {
-                    warnings.innerHTML = `<p id="error">Movie not found. Please check your syntax!</p>`;
-                    warnings.style.display = 'block';
-                    suggestions.style.display = 'none';
+                        // Handle other results
+                    } else if (data.Error === "Too many results.") {
+                        warnings.innerHTML = `<p id="error">Too many results. Please check your syntax!</p>`;
+                        warnings.style.display = 'block';
+                        suggestions.style.display = 'none';
+                    } else if (data.Error === "Movie not found!") {
+                        warnings.innerHTML = `<p id="error">Movie not found. Please check your syntax!</p>`;
+                        warnings.style.display = 'block';
+                        suggestions.style.display = 'none';
+                    } else {
+                        suggestions.style.display = 'none';
+                    }
+                })
+                .catch(error => console.error('Error fetching movie data:', error));
+        } else {
+            suggestions.style.display = 'none';
+        }
+    });
+
+    function selectMovie(imdbID) {
+        const apiUrl = `https://www.omdbapi.com/?i=${encodeURIComponent(imdbID)}&apikey=${OMDB_API_KEY}`;
+        fetch(apiUrl)
+            .then(response => response.json())
+            .then(data => {
+                if (data.Response === "True") {
+                    const movieDataString = JSON.stringify(data);
+                    window.location.href = 'movie-info.html?movieData=' + encodeURIComponent(movieDataString);
                 } else {
-                    suggestions.style.display = 'none';
+                    alert('Movie not found!');
                 }
             })
             .catch(error => console.error('Error fetching movie data:', error));
-    } else {
-        suggestions.style.display = 'none';
     }
-});
-
-function selectMovie(imdbID) {
-    const apiUrl = `https://www.omdbapi.com/?i=${encodeURIComponent(imdbID)}&apikey=${OMDB_API_KEY}`;
-    fetch(apiUrl)
-        .then(response => response.json())
-        .then(data => {
-            if (data.Response === "True") {
-                const movieDataString = JSON.stringify(data);
-                window.location.href = 'movie-info.html?movieData=' + encodeURIComponent(movieDataString);
-            } else {
-                alert('Movie not found!');
-            }
-        })
-        .catch(error => console.error('Error fetching movie data:', error));
-}
 
 
-// Fetches data and updates the content of each movie card on the homepage
-function createMovieCard(cardImageClass, cardTitleClass, imdbClass, rottenClass, metacriticClass) {
-    const cardImage = document.querySelector(cardImageClass);
-    const cardTitle = document.querySelector(cardTitleClass).innerText;
+    // Fetches data and updates the content of each movie card on the homepage
+    function createMovieCard(cardImageClass, cardTitleClass, imdbClass, rottenClass, metacriticClass, cardLinkClass) {
+        const cardImage = document.querySelector(cardImageClass);
+        const cardTitle = document.querySelector(cardTitleClass).innerText;
+        const cardLink = document.querySelector(cardLinkClass);
 
-    let apiUrl = `https://www.omdbapi.com/?t=${encodeURIComponent(cardTitle)}&apikey=${OMDB_API_KEY}`;
+        let apiUrl = `https://www.omdbapi.com/?t=${encodeURIComponent(cardTitle)}&apikey=${OMDB_API_KEY}`;
 
-    fetch(apiUrl)
-        .then(response => response.json())
-        .then(data => {
-            if (data.Response === "True") {
-                const imdbRating = data.Ratings.find(r => r.Source === "Internet Movie Database")?.Value || "N/A";
-                const rottenTomatoesRating = data.Ratings.find(r => r.Source === "Rotten Tomatoes")?.Value || "N/A";
-                const metacriticRating = data.Ratings.find(r => r.Source === "Metacritic")?.Value || "N/A";
+        fetch(apiUrl)
+            .then(response => response.json())
+            .then(data => {
+                if (data.Response === "True") {
+                    const imdbRating = data.Ratings.find(r => r.Source === "Internet Movie Database")?.Value || "N/A";
+                    const rottenTomatoesRating = data.Ratings.find(r => r.Source === "Rotten Tomatoes")?.Value || "N/A";
+                    const metacriticRating = data.Ratings.find(r => r.Source === "Metacritic")?.Value || "N/A";
 
-                // Assign the ratings to the correct elements of the corresponding card
-                document.querySelector(imdbClass).innerText += imdbRating;
-                document.querySelector(rottenClass).textContent += rottenTomatoesRating;
-                document.querySelector(metacriticClass).textContent += metacriticRating;
+                    // Assign the ratings to the correct elements of the corresponding card
+                    document.querySelector(imdbClass).innerText += imdbRating;
+                    document.querySelector(rottenClass).textContent += rottenTomatoesRating;
+                    document.querySelector(metacriticClass).textContent += metacriticRating;
 
-                cardImage.src = data.Poster;
+                    // Update the card image source with the poster
+                    cardImage.src = data.Poster;
 
-            } else {
-                console.error(`Error fetching data for ${cardTitle}:`, data.Error);
-            }
-        })
-        .catch(error => console.error('Error fetching poster card data:', error));
-}
+                    // Set the href attribute of the card link
+                    cardLink.href = 'movie-info.html?movieData=' + encodeURIComponent(JSON.stringify(data));
 
-// Array of card image and title class pairs
-const cardClasses = [
-    {
-        imageClass: '.first-card-img',
-        titleClass: '.first-card-title',
-        imdbClass: '.first-card-imdbRating',
-        rottenClass: '.first-card-rottenTomatoesRating',
-        metacriticClass: '.first-card-metacriticRating'
-    },
-    {
-        imageClass: '.second-card-img',
-        titleClass: '.second-card-title',
-        imdbClass: '.second-card-imdbRating',
-        rottenClass: '.second-card-rottenTomatoesRating',
-        metacriticClass: '.second-card-metacriticRating'
-    },
-    {
-        imageClass: '.third-card-img',
-        titleClass: '.third-card-title',
-        imdbClass: '.third-card-imdbRating',
-        rottenClass: '.third-card-rottenTomatoesRating',
-        metacriticClass: '.third-card-metacriticRating'
-    },
-    {
-        imageClass: '.fourth-card-img',
-        titleClass: '.fourth-card-title',
-        imdbClass: '.fourth-card-imdbRating',
-        rottenClass: '.fourth-card-rottenTomatoesRating',
-        metacriticClass: '.fourth-card-metacriticRating'
+
+                } else {
+                    console.error(`Error fetching data for ${cardTitle}:`, data.Error);
+                }
+            })
+            .catch(error => console.error('Error fetching poster card data:', error));
     }
-];
 
-cardClasses.forEach(card => {
-    createMovieCard(card.imageClass, card.titleClass, card.imdbClass, card.rottenClass, card.metacriticClass);
+    // Array of card image, title, and link class pairs
+    const cardClasses = [
+        {
+            imageClass: '.first-card-img',
+            titleClass: '.first-card-title',
+            imdbClass: '.first-card-imdbRating',
+            rottenClass: '.first-card-rottenTomatoesRating',
+            metacriticClass: '.first-card-metacriticRating',
+            cardLinkClass: '.first-card-link'
+        },
+        {
+            imageClass: '.second-card-img',
+            titleClass: '.second-card-title',
+            imdbClass: '.second-card-imdbRating',
+            rottenClass: '.second-card-rottenTomatoesRating',
+            metacriticClass: '.second-card-metacriticRating',
+            cardLinkClass: '.second-card-link'
+        },
+        {
+            imageClass: '.third-card-img',
+            titleClass: '.third-card-title',
+            imdbClass: '.third-card-imdbRating',
+            rottenClass: '.third-card-rottenTomatoesRating',
+            metacriticClass: '.third-card-metacriticRating',
+            cardLinkClass: '.third-card-link'
+        },
+        {
+            imageClass: '.fourth-card-img',
+            titleClass: '.fourth-card-title',
+            imdbClass: '.fourth-card-imdbRating',
+            rottenClass: '.fourth-card-rottenTomatoesRating',
+            metacriticClass: '.fourth-card-metacriticRating',
+            cardLinkClass: '.fourth-card-link'
+        }
+    ];
+
+    cardClasses.forEach(card => {
+        createMovieCard(card.imageClass, card.titleClass, card.imdbClass, card.rottenClass, card.metacriticClass, card.cardLinkClass);
+    });
+
 });
