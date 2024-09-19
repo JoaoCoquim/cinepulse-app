2135174243de2dc891d83d01cd6a7627b9e70778
JoaoCoquim <joaocoquim@gmail.com>
2024-09-19 15:31:07 +0100

    Added KEYS

--- js/api-keys.js
+++ js/api-keys.js
@@ -1,6 +1,6 @@
 const config = {
-  OMDB_API_KEY: process.env.OMDB_API_KEY,
-  YOUTUBE_API_KEY: process.env.YOUTUBE_API_KEY
+  OMDB_API_KEY: "1f70a320",
+  YOUTUBE_API_KEY: "AIzaSyAlHIWw778In_HmcyQzAkxNOYGhT7l93Jc"
 };
 
 window.config = config; // Expose to global scope
