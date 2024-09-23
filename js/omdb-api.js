3894395e9a0882cedaec5c880a5f47bfe7320f65
JoaoCoquim <joaocoquim@gmail.com>
2024-09-23 19:46:10 +0100

    Added movie Year to the card creation and improved code logic

--- js/omdb-api.js
+++ js/omdb-api.js
@@ -106,33 +106,37 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
     // Fetches data and updates the content of each movie card on the homepage
-    function createMovieCard(movieTitle, cardImageClass, imdbClass, rottenClass, metacriticClass, cardLinkClass) {
-        const cardImage = document.querySelector(cardImageClass);
-        const cardLink = document.querySelector(cardLinkClass);
+    function createMovieCard(data, card) {
+        const movieTitlesArray = Object.keys(data);
+        const movieTitle = movieTitlesArray[Math.floor(Math.random() * movieTitlesArray.length)];
+
         let apiUrl = `https://www.omdbapi.com/?t=${encodeURIComponent(movieTitle)}&apikey=${OMDB_API_KEY}`;
 
         fetch(apiUrl)
             .then(response => response.json())
             .then(movieData => {
                 if (movieData.Response === "True") {
+                    // Set movie title and year
+                    document.querySelector(card.titleClass).innerText = `${movieTitle} (${movieData.Year})`;
+
+                    // Set poster and links
+                    document.querySelector(card.imageClass).src = movieData.Poster;
+                    document.querySelector(card.linkClass).href = 'movie-info.html?movieData=' + encodeURIComponent(JSON.stringify(movieData));
+
                     // Set ratings
                     const imdbRating = movieData.Ratings.find(r => r.Source === "Internet Movie Database")?.Value || "N/A";
                     const rottenTomatoesRating = movieData.Ratings.find(r => r.Source === "Rotten Tomatoes")?.Value || "N/A";
                     const metacriticRating = movieData.Ratings.find(r => r.Source === "Metacritic")?.Value || "N/A";
 
                     // Assign the ratings to the correct elements of the corresponding card
-                    document.querySelector(imdbClass).innerHTML += imdbRating;
-                    document.querySelector(rottenClass).innerHTML += rottenTomatoesRating;
-                    document.querySelector(metacriticClass).innerHTML += metacriticRating;
-
-                    // Update card poster and links
-                    cardImage.src = movieData.Poster;
-                    cardLink.href = 'movie-info.html?movieData=' + encodeURIComponent(JSON.stringify(movieData));
+                    document.querySelector(card.imdbClass).innerHTML += imdbRating;
+                    document.querySelector(card.rottenClass).innerHTML += rottenTomatoesRating;
+                    document.querySelector(card.metacriticClass).innerHTML += metacriticRating;
 
                     // Set links to external rating sites
-                    document.querySelector(imdbClass).href = `https://www.imdb.com/title/${movieData.imdbID}`;
-                    document.querySelector(rottenClass).href = `https://www.rottentomatoes.com/search?search=${movieData.Title}`;
-                    document.querySelector(metacriticClass).href = `https://www.metacritic.com/search/${movieData.Title}`;
+                    document.querySelector(card.imdbClass).href = `https://www.imdb.com/title/${movieData.imdbID}`;
+                    document.querySelector(card.rottenClass).href = `https://www.rottentomatoes.com/search?search=${movieData.Title}`;
+                    document.querySelector(card.metacriticClass).href = `https://www.metacritic.com/search/${movieData.Title}`;
 
                 } else {
                     console.error(`Failed to fetch data for ${movieTitle}: ${movieData.Error}`);
@@ -148,7 +152,7 @@ document.addEventListener('DOMContentLoaded', () => {
             imdbClass: '.first-card-imdbRating',
             rottenClass: '.first-card-rottenTomatoesRating',
             metacriticClass: '.first-card-metacriticRating',
-            cardLinkClass: '.first-card-link'
+            linkClass: '.first-card-link'
         },
         {
             imageClass: '.second-card-img',
@@ -156,7 +160,7 @@ document.addEventListener('DOMContentLoaded', () => {
             imdbClass: '.second-card-imdbRating',
             rottenClass: '.second-card-rottenTomatoesRating',
             metacriticClass: '.second-card-metacriticRating',
-            cardLinkClass: '.second-card-link'
+            linkClass: '.second-card-link'
         },
         {
             imageClass: '.third-card-img',
@@ -164,7 +168,7 @@ document.addEventListener('DOMContentLoaded', () => {
             imdbClass: '.third-card-imdbRating',
             rottenClass: '.third-card-rottenTomatoesRating',
             metacriticClass: '.third-card-metacriticRating',
-            cardLinkClass: '.third-card-link'
+            linkClass: '.third-card-link'
         },
         {
             imageClass: '.fourth-card-img',
@@ -172,21 +176,16 @@ document.addEventListener('DOMContentLoaded', () => {
             imdbClass: '.fourth-card-imdbRating',
             rottenClass: '.fourth-card-rottenTomatoesRating',
             metacriticClass: '.fourth-card-metacriticRating',
-            cardLinkClass: '.fourth-card-link'
+            linkClass: '.fourth-card-link'
         }
     ];
 
     fetch('../data/top100movies.json')
         .then(response => response.json())
         .then(data => {
-            const movieTitlesArray = Object.keys(data);
-
             cardClasses.forEach(card => {
-                const movieTitle = movieTitlesArray[Math.floor(Math.random() * movieTitlesArray.length)];
-                document.querySelector(card.titleClass).innerText = movieTitle; //fills the card with corresponding movie title
-
-                createMovieCard(movieTitle, card.imageClass, card.imdbClass, card.rottenClass, card.metacriticClass, card.cardLinkClass);
+                createMovieCard(data, card);
             });
         })
-        .catch(error => console.error('Error fetching JSON:', error));
+        .catch(error => console.error('Error fetching JSON file:', error));
 });
