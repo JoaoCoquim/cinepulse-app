19d1c1fe9c70d34324bc03d34e01c49817f22ea7
JoaoCoquim <joaocoquim@gmail.com>
2024-10-01 14:27:41 +0100

    Added thumbnail overlay and play button functionality

--- js/youtube-api.js
+++ js/youtube-api.js
@@ -3,7 +3,12 @@ document.addEventListener('DOMContentLoaded', () => {
     const movieTitle = document.getElementById('title').textContent;
     const movieYear = document.getElementById('year').textContent;
     const youtubeSearch = `${movieTitle} ${movieYear} movie trailer`;
+    const imageContainer = document.querySelector('.image-container');
     const placeholderImage = document.getElementById('placeholderImage');
+    const playButtonOverlay = document.querySelector('.play-button-overlay');
+    const trailerWrapper = document.querySelector('.trailer-wrapper');
+    const trailerPlayer = document.getElementById('trailerPlayer');
+    
 
     fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&q=${youtubeSearch}&key=${YOUTUBE_API_KEY}`)
         .then(response => response.json())
@@ -12,24 +17,22 @@ document.addEventListener('DOMContentLoaded', () => {
                 if (data.error.errors[0].reason === 'quotaExceeded') {
                     console.error('Quota limit reached:', data.error.message);
                     placeholderImage.src = 'img/no-trailer-available.jpg'; // Display placeholder image
-                    placeholderImage.alt = `No trailer available thumbnail`;
+                    placeholderImage.alt = `No trailer thumbnail`;
                 } else {
                     console.error('YouTube API Error:', data.error.message);
                 }
             } else {
                 // Check if there are video results
                 if (data.items && data.items.length > 0) {
-                    const trailerWrapper = document.querySelector('.trailer-wrapper');
-                    const trailerPlayer = document.getElementById('trailerPlayer');
                     const videoId = data.items[0].id.videoId;
 
                     placeholderImage.src = data.items[0].snippet.thumbnails.medium.url; // Display trailer thumbnail
                     placeholderImage.alt = `${movieTitle} trailer thumbnail`;
 
-                    placeholderImage.addEventListener('click', () => {
+                    imageContainer.addEventListener('click', () => {
                         placeholderImage.style.display = 'none';
+                        playButtonOverlay.style.display = 'none';
                         trailerWrapper.style.display = 'block';
-
                         trailerPlayer.src = `https://www.youtube.com/embed/${videoId}?autoplay=1`;
                     })
                 };
