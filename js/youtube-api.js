f645bb779fc43b332695a1bc5682c2d8ecbba808
JoaoCoquim <joaocoquim@gmail.com>
2024-10-01 13:52:55 +0100

    Added a thumbnail image before trailers show up and another to failed API calls

--- js/youtube-api.js
+++ js/youtube-api.js
@@ -1,25 +1,41 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const YOUTUBE_API_KEY = window.config.YOUTUBE_API_KEY;
-  const movieTitle = document.getElementById('title').textContent;
-  const movieYear = document.getElementById('year').textContent;
-  const youtubeSearch = `${movieTitle} ${movieYear} movie trailer`;
+    const YOUTUBE_API_KEY = window.config.YOUTUBE_API_KEY;
+    const movieTitle = document.getElementById('title').textContent;
+    const movieYear = document.getElementById('year').textContent;
+    const youtubeSearch = `${movieTitle} ${movieYear} movie trailer`;
+    const placeholderImage = document.getElementById('placeholderImage');
 
-  fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&q=${youtubeSearch}&key=${YOUTUBE_API_KEY}`)
-      .then(response => response.json())
-      .then(data => {
-          if (data.error) {
-              // Handle the error if the response contains an error
-              console.error('YouTube API Error:', data.error.message);
-          } else {
-              // Check if there are video results
-              if (data.items && data.items.length > 0) {
-                  const videoId = data.items[0].id.videoId;  // Gets the first video result
-                  document.getElementById('trailerPlayer').src = `https://www.youtube.com/embed/${videoId}`;
-              }
-          }
-      })
-      .catch(error => {
-          // Catch and handle any errors that occur during the fetch
-          console.error('Error fetching YouTube data:', error);
-      });
+    fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&q=${youtubeSearch}&key=${YOUTUBE_API_KEY}`)
+        .then(response => response.json())
+        .then(data => {
+            if (data.error) {
+                if (data.error.errors[0].reason === 'quotaExceeded') {
+                    console.error('Quota limit reached:', data.error.message);
+                    placeholderImage.src = 'img/no-trailer-available.jpg'; // Display placeholder image
+                    placeholderImage.alt = `No trailer available thumbnail`;
+                } else {
+                    console.error('YouTube API Error:', data.error.message);
+                }
+            } else {
+                // Check if there are video results
+                if (data.items && data.items.length > 0) {
+                    const trailerWrapper = document.querySelector('.trailer-wrapper');
+                    const trailerPlayer = document.getElementById('trailerPlayer');
+                    const videoId = data.items[0].id.videoId;
+
+                    placeholderImage.src = data.items[0].snippet.thumbnails.medium.url; // Display trailer thumbnail
+                    placeholderImage.alt = `${movieTitle} trailer thumbnail`;
+
+                    placeholderImage.addEventListener('click', () => {
+                        placeholderImage.style.display = 'none';
+                        trailerWrapper.style.display = 'block';
+
+                        trailerPlayer.src = `https://www.youtube.com/embed/${videoId}?autoplay=1`;
+                    })
+                };
+            }
+        })
+        .catch(error => {
+            console.error('Error fetching YouTube data:', error);
+        });
 });
