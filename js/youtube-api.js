d81ebc359a00cf3c4918765ee57ead6cf1f6987b
JoaoCoquim <joaocoquim@gmail.com>
2024-10-04 15:27:06 +0100

    Refactored trailer play button logic

--- js/youtube-api.js
+++ js/youtube-api.js
@@ -8,7 +8,6 @@ document.addEventListener('DOMContentLoaded', () => {
     const playButtonOverlay = document.querySelector('.play-button-overlay');
     const trailerWrapper = document.querySelector('.trailer-wrapper');
     const trailerPlayer = document.getElementById('trailerPlayer');
-    
 
     fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&q=${youtubeSearch}&key=${YOUTUBE_API_KEY}`)
         .then(response => response.json())
@@ -16,10 +15,10 @@ document.addEventListener('DOMContentLoaded', () => {
             if (data.error) {
                 if (data.error.errors[0].reason === 'quotaExceeded') {
                     console.error('Quota limit reached:', data.error.message);
-                    placeholderImage.src = 'img/no-trailer-available.jpg'; // Display placeholder image
-                    placeholderImage.alt = `No trailer thumbnail`;
+                    showNotAvailableImage();
                 } else {
                     console.error('YouTube API Error:', data.error.message);
+                    showNotAvailableImage();
                 }
             } else {
                 // Check if there are video results
@@ -28,6 +27,9 @@ document.addEventListener('DOMContentLoaded', () => {
 
                     placeholderImage.src = data.items[0].snippet.thumbnails.medium.url; // Display trailer thumbnail
                     placeholderImage.alt = `${movieTitle} trailer thumbnail`;
+                    placeholderImage.addEventListener('load', () => {
+                        playButtonOverlay.style.display = 'block';
+                    });
 
                     imageContainer.addEventListener('click', () => {
                         placeholderImage.style.display = 'none';
@@ -41,4 +43,11 @@ document.addEventListener('DOMContentLoaded', () => {
         .catch(error => {
             console.error('Error fetching YouTube data:', error);
         });
+
+    const showNotAvailableImage = () => {
+        placeholderImage.src = 'img/no-trailer-available.jpg';
+        placeholderImage.alt = `No trailer thumbnail`;
+        playButtonOverlay.style.display = 'none';
+        imageContainer.style.cursor = 'not-allowed';
+    }
 });
